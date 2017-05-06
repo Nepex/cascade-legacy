cd307eb6ee6c344d7c776855747d0bab13a32333
nepex <thechump@hotmail.com>
2017-05-05 22:22:05 -0500

    mobile cleanup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,39 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { SessionService, SessionStateService } from '../api/index';
 
 @Component({
-  selector: 'cascade-home',
-  templateUrl: './home.html',
-  styleUrls: ['./home.css']
+    selector: 'cascade-home',
+    templateUrl: './home.html',
+    styleUrls: ['./home.css'],
+    animations: [
+        trigger('visibilityChanged', [
+            state('hidden', style({ opacity: 0 })),
+            state('shown', style({ opacity: 1 })),
+
+            transition('hidden => shown', animate('1000ms ease-in'))
+        ])
+    ]
 })
 export class HomeComponent {
 
-  constructor(private router: Router, private sessionStateService: SessionStateService, private sessionService: SessionService,
+    state = 'hidden';
+
+    constructor(private router: Router, private sessionStateService: SessionStateService, private sessionService: SessionService,
         private activatedRoute: ActivatedRoute) {
         this.activate();
     }
 
-  activate() {
+    activate() {
         if (this.sessionStateService.isAuthenticated()) {
             this.router.navigateByUrl('/party');
         }
+
+        setTimeout(() => {
+            this.state = 'shown';
+        }, 50);
     }
 
 }
